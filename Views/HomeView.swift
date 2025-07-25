944c39fd443df1f6804d2ef9117082a6ab73621c
Kyle Morton <mkmorton91@gmail.com>
2025-07-25 18:01:37 -0500

    Setting up reference in the plist for api key, testing that it's setup correctly w/ a print()

--- Views/HomeView.swift
+++ Views/HomeView.swift
@@ -13,6 +13,10 @@ struct HomeView: View {
             FeedView()
         }
         .padding()
+        .onAppear(perform: {
+            print(Bundle.main.infoDictionary?["API_KEY"] as? String
+                  ?? "Cant find it!")
+        })
     }
 }
 
