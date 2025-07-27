65052ef486ac3374a5c8e495c72c60966308071b
Kyle Morton <mkmorton91@gmail.com>
2025-07-27 09:43:09 -0500

    Video detail view working, need to add in package to play the video on the sheet

--- Views/FeedView.swift
+++ Views/FeedView.swift
@@ -10,16 +10,25 @@ import SwiftUI
 struct FeedView: View {
     
     @State private var videos: [Video] = []
+    @State private var selectedVideo: Video?
     
     var body: some View {
         List(videos) { video in
-            VideoRowView(video: video)
+            if (video.getThumbnailUrl() != "") {
+                VideoRowView(video: video)
+                    .onTapGesture {
+                        selectedVideo = video
+                    }
+            }
         }
         .listStyle(.plain)
         .scrollIndicators(.hidden)
         .task { // lets you run an async section of code as a background worker
             let videos = await DataService().getVideos()
             self.videos = videos
+        }
+        .sheet(item: $selectedVideo) { selectedVid in // opened whenever this var has a value
+            VideoDetailView(video: selectedVid)
         }
     }
 }
--- Views/VideoDetailView.swift
+++ Views/VideoDetailView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct VideoDetailView: View {
+    
+    var video: Video
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            // Video player
+            
+            // Title and description
+            ScrollView {
+                VStack(alignment: .leading, spacing: 30) {
+                    Text(video.snippet?.title ?? "")
+                        .font(.headline)
+                        .bold()
+                    
+                    Text(video.snippet?.description ?? "")
+                }
+            }
+            .scrollIndicators(.hidden)
+            .padding()
+        }
     }
 }
 
-#Preview {
-    VideoDetailView()
-}
+//#Preview {
+//    VideoDetailView(video: Video(id: "123"))
+//    Spacer()
+//}
